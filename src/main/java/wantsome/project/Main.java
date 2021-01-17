001a51f1aa2e86b588e4eb5afbcf4d43da6542e3
Munca <andreires99@gmail.com>
2021-01-17 21:43:16 +0200

    Final Project

--- src/main/java/wantsome/project/Main.java
+++ src/main/java/wantsome/project/Main.java
@@ -1,7 +1,11 @@
 package wantsome.project;
 
 import spark.Spark;
+import wantsome.project.DTO.UserDTO;
+import wantsome.project.Model.User;
+import wantsome.project.Model.UserType;
 import wantsome.project.web.Paths;
+
 import static spark.Spark.*;
 import static spark.debug.DebugScreen.*;
 import static wantsome.project.Controller.AboutController.getAboutPage;
@@ -17,14 +21,16 @@ import static wantsome.project.Controller.UserController.*;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         enableDebugScreen();
         setup();
         configureRoutesAndStart();
+
     }
 
-    private static void setup() {
-
+    public static void setup() {
+        UserDTO userAdmin = new UserDTO(1,"admin@gmail.com","password", UserType.ADMIN,"Andrei","My address","0722443322");
+        UserDTO userCustomer = new UserDTO(1,"example@gmail.com","password", UserType.CUSTOMER,"Andrei","My address","0722443322");
     }
 
     private static void configureRoutesAndStart() {
@@ -45,21 +51,20 @@ public class Main {
         get(Paths.Web.PRODUCTPAGE, getEachProductsPage);
         get(Paths.Web.ABOUTPAGE, getAboutPage);
         get(Paths.Web.CARTPAGE, getCartPage);
-        get(Paths.Web.ACCSETTINGS,getAccSet);
-        get(Paths.Web.CARTPAGED,removeItem);
+        get(Paths.Web.ACCSETTINGS, getAccSet);
+        get(Paths.Web.CARTPAGED, removeItem);
         get(Paths.Web.LOGORREG, LogOrReg);
-        get(Paths.Web.USERORDERS,getOrdersPage);
-        get(Paths.Web.ORDERS,getOrderList);
+        get(Paths.Web.USERORDERS, getOrdersPage);
+        get(Paths.Web.ORDERS, getOrderList);
 
-
-        post(Paths.Web.CARTPAGE,finishOrder);
+        post(Paths.Web.CARTPAGE, finishOrder);
         post(Paths.Web.REGISTER, handleRegisterPost);
         post(Paths.Web.LOGIN, handleLoginPost);
         post(Paths.Web.LOGOUT, handleLogoutPost);
         post(Paths.Web.ADMINPANEL, handleProductPost);
         post(Paths.Web.EDTPRODADMIN2, getEditProductAdminPost);
         post(Paths.Web.PRODUCTPAGE, addToCart);
-        post(Paths.Web.ACCSETTINGS,changeAccSet);
+        post(Paths.Web.ACCSETTINGS, changeAccSet);
 
         awaitInitialization();
         System.out.println("\nServer started: http://localhost:4567/main");
